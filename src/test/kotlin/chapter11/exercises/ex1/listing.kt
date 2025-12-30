f2cd349d76a235886f4e317cb43b4dfc0a0b7ebf
sjm150 <sjm150@snu.ac.kr>
2025-12-30 12:02:27 +0900

    do chapter11 exercises 1~2

--- src/test/kotlin/chapter11/exercises/ex1/listing.kt
+++ src/test/kotlin/chapter11/exercises/ex1/listing.kt
@@ -1,34 +1,72 @@
 package chapter11.exercises.ex1
 
+import arrow.Kind
 import arrow.core.ForListK
 import arrow.core.ForSequenceK
+import arrow.core.ListK
+import arrow.core.SequenceK
+import arrow.core.fix
 import chapter10.ForList
 import chapter10.ForOption
+import chapter10.List
+import chapter10.Some
+import chapter10.fix
 import chapter11.ForPar
+import chapter11.Par
+import chapter11.fix
 import chapter11.sec2.Monad
-import utils.SOLUTION_HERE
 
 //tag::init1[]
 object Monads {
 
-    fun parMonad(): Monad<ForPar> =
+    fun parMonad(): Monad<ForPar> = object : Monad<ForPar> {
+        override fun <A> unit(a: A): Kind<ForPar, A> = Par.unit(a)
 
-        SOLUTION_HERE()
+        override fun <A, B> flatMap(
+            fa: Kind<ForPar, A>,
+            f: (A) -> Kind<ForPar, B>
+        ): Kind<ForPar, B> =
+            fa.fix().flatMap { f(it).fix() }
+    }
 
-    fun optionMonad(): Monad<ForOption> =
+    fun optionMonad(): Monad<ForOption> = object : Monad<ForOption> {
+        override fun <A> unit(a: A): Kind<ForOption, A> = Some(a)
 
-        SOLUTION_HERE()
+        override fun <A, B> flatMap(
+            fa: Kind<ForOption, A>,
+            f: (A) -> Kind<ForOption, B>
+        ): Kind<ForOption, B> =
+            fa.fix().flatMap { f(it).fix() }
+    }
 
-    fun listMonad(): Monad<ForList> =
+    fun listMonad(): Monad<ForList> = object : Monad<ForList> {
+        override fun <A> unit(a: A): Kind<ForList, A> = List.of(a)
 
-        SOLUTION_HERE()
+        override fun <A, B> flatMap(
+            fa: Kind<ForList, A>,
+            f: (A) -> Kind<ForList, B>
+        ): Kind<ForList, B> =
+            fa.fix().flatMap { f(it).fix() }
+    }
 
-    fun listKMonad(): Monad<ForListK> =
+    fun listKMonad(): Monad<ForListK> = object : Monad<ForListK> {
+        override fun <A> unit(a: A): Kind<ForListK, A> = ListK.just(a)
 
-        SOLUTION_HERE()
+        override fun <A, B> flatMap(
+            fa: Kind<ForListK, A>,
+            f: (A) -> Kind<ForListK, B>
+        ): Kind<ForListK, B> =
+            fa.fix().flatMap { f(it).fix() }
+    }
 
-    fun sequenceKMonad(): Monad<ForSequenceK> =
+    fun sequenceKMonad(): Monad<ForSequenceK> = object : Monad<ForSequenceK> {
+        override fun <A> unit(a: A): Kind<ForSequenceK, A> = SequenceK.just(a)
 
-        SOLUTION_HERE()
+        override fun <A, B> flatMap(
+            fa: Kind<ForSequenceK, A>,
+            f: (A) -> Kind<ForSequenceK, B>
+        ): Kind<ForSequenceK, B> =
+            fa.fix().flatMap { f(it).fix() }
+    }
 }
 //end::init1[]
--- src/test/kotlin/chapter11/exercises/ex2/listing.kt
+++ src/test/kotlin/chapter11/exercises/ex2/listing.kt
@@ -1,7 +1,22 @@
 package chapter11.exercises.ex2
 
-import chapter11.solutions.ex2.StateOf
+import arrow.Kind
+import arrow.Kind2
+import chapter11.Monad
 
 //tag::init[]
-data class State<S, out A>(val run: (S) -> Pair<A, S>) : StateOf<S, A>
-//end::init[]
+data class State<S, out A>(val run: (S) -> Pair<A, S>) : Kind2<ForState, S, A>
+
+class ForState {}
+
+fun <S, A> Kind2<ForState, S, A>.fix(): State<S, A> = this as State<S, A>
+
+interface StateMonad<S> : Monad<Kind<ForState, S>> {
+    override fun <A> unit(a: A): Kind2<ForState, S, A>
+
+    override fun <A, B> flatMap(
+        fa: Kind2<ForState, S, A>,
+        f: (A) -> Kind2<ForState, S, B>
+    ): Kind2<ForState, S, B>
+}
+
